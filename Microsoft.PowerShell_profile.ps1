e649e53084fe553f526c749c43413474158dbbf8
Joash Christian Liwanag <nightlock0326@gmail.com>
2020-10-21 17:49:01 +0800

    Add Powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,61 @@
+# ssh functions
+function akagi{
+	ssh -p 1991 elskiee@192.168.18.144
+}
+function akagi-pub{
+	ssh -p 1991 elskiee@kaga-cloud.ddns.net
+}
+function kaga{
+	ssh nightlock0326@kaga-cloud.ddns.net
+}
+function pwsh-a{
+	Start-Process pwsh -Verb RunAs
+}
+function cmd-a{
+	Start-Process cmd -Verb RunAs
+}
+function ln ($target, $link) {
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+function upgd{
+	param(
+		[array] $AppList
+	)
+	ForEach ($App in $AppList) {
+		scoop update $App
+	}
+}
+function inst{
+	param(
+		[array] $AppList
+	)
+	ForEach ($App in $AppList) {
+		scoop install $App
+	}
+}
+function remo{
+	param(
+		[array] $AppList
+	)
+	ForEach ($App in $AppList) {
+		scoop uninstall $App
+	}
+}
+function updt{
+	scoop update && scoop status
+}
+
+# colored ls output
+If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
+    Import-Module Get-ChildItemColor
+    
+    Set-Alias la Get-ChildItem -option AllScope
+    Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+}
+
+function prompt {
+    Write-Host "PSC $(Get-Location) "
+    return "> "
+}
+
+Set-Alias make mingw32-make
