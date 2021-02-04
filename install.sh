21ed827b3c12d2c619c73f771ddac2b1caaa2865
Joash Christian Liwanag <nightlock0326@gmail.com>
2021-02-05 01:24:41 +0800

    Change discord install

--- install.sh
+++ install.sh
@@ -78,8 +78,6 @@ elif [ "$OS" == "Fedora" ]; then
 	sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 	### Add themes copr
 	sudo dnf copr enable tcg/themes -y
-	### Add Discord copr
-	sudo dnf copr enable tcg/discord -y
 	### Add vscode repo
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'	
@@ -87,8 +85,11 @@ elif [ "$OS" == "Fedora" ]; then
 	sudo dnf check-update
 	## Install Essential Packages
 	sudo dnf -y groupinstall "Development Tools"
-	sudo dnf install zsh curl neovim papirus-icon-theme materia-gtk-theme tmux code lpf-spotify-client Discord-installer -y
+	sudo dnf install zsh curl neovim papirus-icon-theme materia-gtk-theme tmux code lpf-spotify-client snapd -y
 	lpf update
+	sudo ln -s /var/lib/snapd/snap /snap
+	### Install Discord
+	sudo snap install discord
 fi
 
 # Clone the dotfiles git if it's not found
