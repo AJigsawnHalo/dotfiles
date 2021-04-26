3264e1e87421f5693489e6893d30f1d84d26787b
Joash Christian L <nightlock0326@gmail.com>
2021-04-26 17:28:28 +0800

    Remove copr repo.

--- install.sh
+++ install.sh
@@ -79,8 +79,6 @@ elif [ "$OS" == "Fedora" ]; then
 	### Add RPM Fusion repos
 	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
 	sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-	### Add themes copr
-	sudo dnf copr enable tcg/themes -y
 	### Add vscode repo
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'	
